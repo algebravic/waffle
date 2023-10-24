b1ac12e4829eb5e218cd4afed862f95ed462e82c
Victor S. Miller <victor.miller@sri.com>
2023-10-24 11:14:08 -0700

    Added newly exposed functions

--- waffle/__init__.py
+++ waffle/__init__.py
@@ -1,3 +1,7 @@
 from .waffle_solve import Waffle
 from .group import minimal_element
+from .cycles import rank_stirling, unrank_stirling
+from .derangements imort associated, unrank_associated, rank_associated
+from .transpositions import cycle_census
+from .sample import cms_sample
 
